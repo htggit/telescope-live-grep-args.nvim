30627c8f46e1811db2053e4b617af3e7499a2499
Gavin Wu <haitaoge@gmail.com>
2023-11-09 09:31:26 +1000

    fix quoted

--- lua/telescope-live-grep-args/helpers.lua
+++ lua/telescope-live-grep-args/helpers.lua
@@ -9,11 +9,15 @@ local quote_default_opts = {
 }
 
 M.quote = function(value, opts)
+  local isQuoted = function(s)
+    return s:match("^" .. opts.quote_char)
+  end
+
   opts = opts or {}
   opts = vim.tbl_extend("force", quote_default_opts, opts)
 
-  local quoted = value:gsub(opts.quote_char, "\\" .. opts.quote_char)
-  return opts.quote_char .. quoted .. opts.quote_char
+  local quoted = isQuoted(value) and value or value:gsub(opts.quote_char, "\\" .. opts.quote_char)
+  return isQuoted(quoted) and quoted or (opts.quote_char .. quoted .. opts.quote_char)
 end
 
 return M
